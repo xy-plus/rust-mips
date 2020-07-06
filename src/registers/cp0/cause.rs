0b4c5e11f4580713c7bc43d40629b49f17dc2263
刘丰源 <liufy17@mails.tsinghua.edu.cn>
2020-07-06 23:06:34 +0800

    change enum to numeric-enum

--- src/registers/cp0/cause.rs
+++ src/registers/cp0/cause.rs
@@ -1,5 +1,8 @@
 //! MIPS CP0 Cause register
 
+use core::convert::TryFrom;
+use numeric_enum_macro::numeric_enum;
+
 #[derive(Clone, Copy, Debug)]
 pub struct Cause {
     pub bits: u32,
@@ -13,41 +16,44 @@ register_set_reset_bit!(set_soft_int1, reset_soft_int1, 9);
 
 register_set_reset_bit!(set_iv, reset_iv, 23);
 
-#[derive(Clone, Copy, Debug)]
-pub enum Exception {
-    Interrupt,
-    TLBModification,
-    TLBLoadMiss,
-    TLBStoreMiss,
-    AddressLoadError,
-    AddressStoreError,
-    BusLoadError,
-    BusStoreError,
-    Syscall,
-    Breakpoint,
-    ReservedInstruction,
-    CoprocessorUnusable,
-    Overflow,
-    TrapException,
-    MSAFloatingPoint,
-    FloatingPoint,
-    Reserved1,
-    Reserved2,
-    Coprocessor2,
-    TLBReadInhibit,
-    TLBExecutionInhibit,
-    MSADisabled,
-    MDMX,
-    Watch,
-    MachineCheck,
-    ThreadException,
-    DSPDisabled,
-    VirtualizedGuest,
-    Reserved3,
-    Reserved4,
-    CacheError,
-    Reserved5,
-    Unknown,
+numeric_enum! {
+    #[repr(u32)]
+    #[derive(Clone, Copy, Debug, PartialEq, PartialOrd, Ord, Eq, Hash)]
+    pub enum Exception {
+        Interrupt = 0,
+        TLBModification = 1,
+        TLBLoadMiss = 2,
+        TLBStoreMiss = 3,
+        AddressLoadError = 4,
+        AddressStoreError = 5,
+        BusLoadError = 6,
+        BusStoreError = 7,
+        Syscall = 8,
+        Breakpoint = 9,
+        ReservedInstruction = 10,
+        CoprocessorUnusable = 11,
+        Overflow = 12,
+        TrapException = 13,
+        MSAFloatingPoint = 14,
+        FloatingPoint = 15,
+        Reserved1 = 16,
+        Reserved2 = 17,
+        Coprocessor2 = 18,
+        TLBReadInhibit = 19,
+        TLBExecutionInhibit = 20,
+        MSADisabled = 21,
+        MDMX = 22,
+        Watch = 23,
+        MachineCheck = 24,
+        ThreadException = 25,
+        DSPDisabled = 26,
+        VirtualizedGuest = 27,
+        Reserved3 = 28,
+        Reserved4 = 29,
+        CacheError = 30,
+        Reserved5 = 31,
+        Unknown = 32,
+    }
 }
 
 #[derive(Clone, Copy, Debug)]
@@ -56,51 +62,11 @@ pub enum SoftwareInterrupt {
     SoftInt1,
 }
 
-impl Exception {
-    pub fn from(exccode: u32) -> Self {
-        match exccode {
-            0 => Exception::Interrupt,
-            1 => Exception::TLBModification,
-            2 => Exception::TLBLoadMiss,
-            3 => Exception::TLBStoreMiss,
-            4 => Exception::AddressLoadError,
-            5 => Exception::AddressStoreError,
-            6 => Exception::BusLoadError,
-            7 => Exception::BusStoreError,
-            8 => Exception::Syscall,
-            9 => Exception::Breakpoint,
-            10 => Exception::ReservedInstruction,
-            11 => Exception::CoprocessorUnusable,
-            12 => Exception::Overflow,
-            13 => Exception::TrapException,
-            14 => Exception::MSAFloatingPoint,
-            15 => Exception::FloatingPoint,
-            16 => Exception::Reserved1,
-            17 => Exception::Reserved2,
-            18 => Exception::Coprocessor2,
-            19 => Exception::TLBReadInhibit,
-            20 => Exception::TLBExecutionInhibit,
-            21 => Exception::MSADisabled,
-            22 => Exception::MDMX,
-            23 => Exception::Watch,
-            24 => Exception::MachineCheck,
-            25 => Exception::ThreadException,
-            26 => Exception::DSPDisabled,
-            27 => Exception::VirtualizedGuest,
-            28 => Exception::Reserved3,
-            29 => Exception::Reserved4,
-            30 => Exception::CacheError,
-            31 => Exception::Reserved5,
-            _ => Exception::Unknown,
-        }
-    }
-}
-
 impl Cause {
     #[inline]
     pub fn cause(&self) -> Exception {
         // exc_code = cause_reg[6..2]
-        Exception::from((self.bits >> 2) & 0x1f)
+        Exception::try_from((self.bits >> 2) & 0x1f).unwrap_or(Exception::Unknown)
     }
 
     #[inline]
@@ -109,5 +75,10 @@ impl Cause {
         let soft_int = (self.bits >> 8) & 0b11;
         let hard_int = (self.bits >> 10) & 0b111_111;
         soft_int | (hard_int << 2)
+    }
+
+    #[inline]
+    pub fn hard_int5(&self) -> bool {
+        ((self.bits >> 14) & 1) == 1
     }
 }
